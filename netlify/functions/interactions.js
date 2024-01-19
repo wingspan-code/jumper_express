5b142086e43ae64f4de43a584c36e67fda9d427b
sebtlm <thelayeredmind@gmail.com>
2024-01-19 19:22:00 +0100

    Fix async calls in Request handling

--- netlify/functions/interactions.js
+++ netlify/functions/interactions.js
@@ -4,8 +4,8 @@ import {Timer, VerifyDiscordInteraction} from '../../lib/utils.js'
 
 export default async (req, context) => {
 	const timer = Timer();
-	return VerifyDiscordInteraction(Netlify.get("DISCORD_KEY"), req, () => {
-		const response_to_discord = await discord(req); 
+	return await VerifyDiscordInteraction(Netlify.get("DISCORD_KEY"), req, () => {
+		const response_to_discord = discord(req); 
 		console.log(`It took ${timer()} to process request ${req.body.id}`);
 		return response_to_discord;
 	})
