b3a6d682cd1f58692ab0bf0902c9a03d15cddb3f
sebtlm <thelayeredmind@gmail.com>
2024-01-19 16:37:11 +0100

    Fix missing module export in deploy successful hook

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,7 +1,9 @@
 // Function to run after successfull deploy automatically to register Discord commands once
 const register = require('../../lib/commands.js')
-if(global.Netlify != undefined){
-	register(Netlify.env.get('APP_ID'), Netlify.env.get('DISCORD_TOKEN'));
-} else {
-	console.log("WARNING: This function requires a Netlify environment!");
+module.exports = () => {
+	try {
+		register(Netlify.env.get('APP_ID'), Netlify.env.get('DISCORD_TOKEN'));
+	} catch {
+		console.log("WARNING: This function requires a Netlify environment!");
+	}
 }
