d74286877b0176361ccddb239e579dfb2467dec9
sebtlm <thelayeredmind@gmail.com>
2024-01-19 19:16:22 +0100

    Add public key decryption to verify discord interaction

--- netlify/functions/interactions.js
+++ netlify/functions/interactions.js
@@ -1,10 +1,12 @@
 // Main API processing discord interactions
 import discord from '../../lib/discord.js'
-import {Timer} from '../../lib/utils.js'
+import {Timer, VerifyDiscordInteraction} from '../../lib/utils.js'
 
 export default async (req, context) => {
 	const timer = Timer();
-	const response_to_discord = await discord(req); 
-	console.log(`It took ${timer()} to process request ${req.body.id}`);
-	return response_to_discord;
+	return VerifyDiscordInteraction(Netlify.get("DISCORD_KEY"), req, () => {
+		const response_to_discord = await discord(req); 
+		console.log(`It took ${timer()} to process request ${req.body.id}`);
+		return response_to_discord;
+	})
 }
